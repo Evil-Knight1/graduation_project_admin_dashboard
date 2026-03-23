541d2d7453c28658f86fa8a1f1d87f6d4a9ab67d
abohussein <110132240+Evil-Knight1@users.noreply.github.com>
2026-03-23 20:23:35 +0200

    init flutter app

---
+++ README.md
@@ -0,0 +1,3 @@
+# graduation_project_admin_dashboard
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/graduation_project_admin_dashboard/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.graduation_project_admin_dashboard
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
